e6108134fcff02bd8ceac195314071e67052a5f1
Alihan Yesil <alihannyesil@gmail.com>
2021-11-24 21:29:10 +0300

    laravel in vue create

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ route::prefix('/')->middleware('isAdmin')->group(function(){
     Route::get('GetMessage',[genelController::class,'index'])->name('index');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
